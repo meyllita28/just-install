f6472cbc3551dd5bd24d92dce46ffff935c2943c
Lorenzo Villani <lorenzo@villani.me>
2020-03-01 13:10:53 +0100

    Add and use registry4 package

--- cmd/just-install/action_install.go
+++ cmd/just-install/action_install.go
@@ -16,13 +16,31 @@
 package main
 
 import (
+	"bytes"
 	"errors"
 	"fmt"
 	"log"
+	"os"
+	"path/filepath"
+	"strings"
+	"text/template"
 
+	"github.com/gotopkg/mslnk/pkg/mslnk"
+	"github.com/ungerik/go-dry"
 	"github.com/urfave/cli/v2"
 
+	"github.com/just-install/just-install/pkg/cmd"
+	"github.com/just-install/just-install/pkg/fetch"
+	"github.com/just-install/just-install/pkg/installer"
+	"github.com/just-install/just-install/pkg/paths"
 	"github.com/just-install/just-install/pkg/platform"
+	"github.com/just-install/just-install/pkg/registry4"
+	"github.com/just-install/just-install/pkg/strings2"
+)
+
+var (
+	shimsPath = os.ExpandEnv("${SystemDrive}\\Shims")
+	startMenu = os.ExpandEnv("${ProgramData}\\Microsoft\\Windows\\Start Menu\\Programs")
 )
 
 func handleInstall(c *cli.Context) error {
@@ -35,30 +53,96 @@ func handleInstall(c *cli.Context) error {
 		return err
 	}
 
-	// Architecture selection
-	arch := c.String("arch")
-	switch arch {
-	case "":
-		if platform.Is64Bit() {
-			arch = "x86_64"
-		} else {
-			arch = "x86"
-		}
-	case "x86":
-		// Nothing to do
-	case "x86_64":
-		if !platform.Is64Bit() {
-			return errors.New("this machine cannot run 64-bit software")
-		}
-	default:
-		return fmt.Errorf("unknown architecture: %v", arch)
+	arch, err := getInstallArch(c.String("arch"))
+	if err != nil {
+		return err
 	}
 
-	// Check which packages might require an interactive installation
-	var interactive []string
+	printInteractivePackages(registry.Packages, c.Args().Slice())
+
+	// Install packages
+	hasErrors := false
 
 	for _, pkg := range c.Args().Slice() {
 		entry, ok := registry.Packages[pkg]
+		if !ok {
+			log.Println("WARNING: unknown package", pkg)
+			continue
+		}
+
+		if onlyShims {
+			// FIXME:
+			// entry.CreateShims(arch)
+			continue
+		}
+
+		installerPath, err := fetchInstaller(entry, arch, force)
+		if err != nil {
+			log.Printf("error downloading %v: %v", pkg, err)
+			hasErrors = true
+			continue
+		}
+
+		if onlyDownload {
+			continue
+		}
+
+		options, err := entry.Installer.OptionsForArch(arch)
+		if err != nil {
+			return err
+		}
+
+		installerPath, err = maybeExtractContainer(installerPath, options)
+		if err != nil {
+			return err
+		}
+
+		if err := install(installerPath, entry.Installer.Kind, options); err != nil {
+			log.Printf("error installing %v: %v", pkg, err)
+			hasErrors = true
+			continue
+		}
+
+	}
+
+	if hasErrors {
+		return errors.New("encountered errors installing packages (see the log for details)")
+	}
+
+	return nil
+}
+
+// getInstallArch returns the architecture selected for package installation based on the given
+// preferred architecture (e.g. given by the user via command line arguments). The given preferred
+// architecture can be empty, in which case a suitable one is automatically selected for the current
+// machine.
+func getInstallArch(preferredArch string) (string, error) {
+	switch preferredArch {
+	case "":
+		if platform.Is64Bit() {
+			return "x86_64", nil
+		}
+
+		return "x86", nil
+	case "x86":
+		return preferredArch, nil
+	case "x86_64":
+		if !platform.Is64Bit() {
+			return "", errors.New("this machine cannot run 64-bit software")
+		}
+
+		return preferredArch, nil
+	default:
+		return "", fmt.Errorf("unknown architecture: %v", preferredArch)
+	}
+}
+
+// printInteractivePackages prints the names of packages that require user interaction.
+func printInteractivePackages(packageMap registry4.PackageMap, requestedPackages []string) {
+	var interactive []string
+
+	for _, pkg := range requestedPackages {
+		entry, ok := packageMap[pkg]
 		if !ok {
 			continue
 		}
@@ -68,41 +152,233 @@ func handleInstall(c *cli.Context) error {
 		}
 	}
 
-	if len(interactive) > 0 {
-		log.Println("these packages might require user interaction to complete their installation")
-
-		for _, pkg := range interactive {
-			log.Println("    " + pkg)
-		}
-
-		log.Println("")
+	if len(interactive) < 1 {
+		return
 	}
 
-	// Install packages
-	hasErrors := false
+	log.Println("these packages might require user interaction to complete their installation")
 
-	for _, pkg := range c.Args().Slice() {
-		entry, ok := registry.Packages[pkg]
+	for _, pkg := range interactive {
+		log.Println("    " + pkg)
+	}
 
-		if ok {
-			if onlyShims {
-				entry.CreateShims(arch)
-			} else if onlyDownload {
-				entry.DownloadInstaller(arch, force)
-			} else {
-				if err := entry.JustInstall(arch, force); err != nil {
-					log.Printf("error installing %v: %v", pkg, err)
-					hasErrors = true
-				}
-			}
+	log.Println("")
+}
+
+// fetchInstaller fetches the installer for the given package and returns
+func fetchInstaller(entry *registry4.Package, arch string, overwrite bool) (string, error) {
+	// Sanity check
+	if isEmptyString(entry.Installer.X86) && isEmptyString(entry.Installer.X86_64) {
+		return "", errors.New("package entry is missing both 32-bit and 64-bit installers")
+	}
+
+	// Pick preferred installer
+	var installerURL string
+	switch arch {
+	case "x86":
+		if isEmptyString(entry.Installer.X86) {
+			return "", errors.New("this package doesn't offer a 32-bit installer")
+		}
+
+		installerURL = entry.Installer.X86
+	case "x86_64":
+		if isEmptyString(entry.Installer.X86_64) {
+			// Fallback to the 32-bit installer
+			installerURL = entry.Installer.X86
 		} else {
-			log.Println("WARNING: unknown package", pkg)
+			installerURL = entry.Installer.X86_64
 		}
+	default:
+		panic("programmer error")
 	}
 
-	if hasErrors {
-		return errors.New("encountered errors installing packages")
+	installerURL, err := expandString(installerURL, map[string]string{"version": entry.Version})
+	if err != nil {
+		return "", fmt.Errorf("could not expand installer URL's template string: %w", err)
 	}
 
-	return nil
+	downloadDir, err := paths.TempDirCreate()
+	if err != nil {
+		return "", fmt.Errorf("could not create temporary directory to download installer: %w", err)
+	}
+
+	ret, err := fetch.Fetch(installerURL, &fetch.Options{
+		Destination: downloadDir,
+		Overwrite:   overwrite,
+		Progress:    true,
+	})
+
+	return ret, err
+}
+
+func maybeExtractContainer(path string, options *registry4.Options) (string, error) {
+	if options == nil || options.Container == nil {
+		return path, nil
+	}
+
+	if strings2.IsEmpty(options.Container.Installer) {
+		return "", errors.New("\"installer\" option cannot be empty")
+	}
+
+	if options.Container.Kind != "zip" {
+		return "", errors.New("only \"zip\" containers are supported")
+	}
+
+	tempDir, err := paths.TempDirCreate()
+	if err != nil {
+		return "", err
+	}
+
+	extractDir := filepath.Join(tempDir, filepath.Base(path)+"_extracted")
+	log.Println("extracting container", path, "to", extractDir)
+	if err := installer.ExtractZIP(path, extractDir); err != nil {
+		return "", err
+	}
+
+	return filepath.Join(extractDir, options.Container.Installer), nil
+}
+
+func install(path string, kind string, options *registry4.Options) error {
+	// One-off, custom, installers
+	switch kind {
+	case "copy":
+		if options == nil {
+			return errors.New("the \"copy\" installer requires additional options")
+		}
+
+		if strings2.IsEmpty(options.Destination) {
+			return errors.New("\"destination\" is missing from installer options")
+		}
+
+		destination, err := expandString(options.Destination, nil)
+		if err != nil {
+			return fmt.Errorf("could not expand destination string: %w", err)
+		}
+
+		parentDir := filepath.Dir(destination)
+		log.Println("creating", parentDir)
+		if err := os.MkdirAll(parentDir, os.ModePerm); err != nil {
+			return err
+		}
+
+		log.Println("copying to", destination)
+		return dry.FileCopy(path, destination)
+	case "custom":
+		if options == nil {
+			return errors.New("the \"custom\" installer requires additional options")
+		}
+
+		if len(options.Arguments) < 1 {
+			return errors.New("\"arguments\" is missing from installer options")
+		}
+
+		var args []string
+		for _, v := range options.Arguments {
+			expanded, err := expandString(v, map[string]string{"installer": path})
+			if err != nil {
+				return err
+			}
+
+			args = append(args, expanded)
+		}
+
+		return cmd.Run(args...)
+	case "zip":
+		if options == nil {
+			return errors.New("the \"zip\" installer requires additional options")
+		}
+
+		if strings2.IsEmpty(options.Destination) {
+			return errors.New("\"destination\" is missing from installer options")
+		}
+
+		destination, err := expandString(options.Destination, nil)
+		if err != nil {
+			return fmt.Errorf("could not expand destination string: %w", err)
+		}
+
+		log.Println("extracting to", destination)
+		if err := installer.ExtractZIP(path, destination); err != nil {
+			return err
+		}
+
+		for _, shortcut := range options.Shortcuts {
+			shortcutName, err := expandString(shortcut.Name, nil)
+			if err != nil {
+				return fmt.Errorf("could not expand shortcut name string template: %w", err)
+			}
+
+			shortcutTarget, err := expandString(shortcut.Target, nil)
+			if err != nil {
+				return fmt.Errorf("could not expand shortcut target string template: %w", err)
+			}
+
+			shortcutLocation := filepath.Join(startMenu, shortcutName+".lnk")
+
+			log.Println("creating shortcut to", shortcutTarget, "in", shortcutLocation)
+			if err := mslnk.LinkFile(shortcutTarget, shortcutLocation); err != nil {
+				return fmt.Errorf("could not create shortcut: %w", err)
+			}
+		}
+
+		return nil
+	}
+
+	// Regular installer
+	installerType := installer.InstallerType(kind)
+	if !installerType.IsValid() {
+		return fmt.Errorf("unknown installer type: %v", kind)
+	}
+
+	installerCommand, err := installer.Command(path, installerType)
+	if err != nil {
+		return err
+	}
+
+	return cmd.Run(installerCommand...)
+}
+
+func isEmptyString(s string) bool {
+	return len(strings.TrimSpace(s)) < 1
+}
+
+// expandString expands any environment variable in the given string, with additional variables
+// coming from the given context.
+func expandString(s string, context map[string]string) (string, error) {
+	data := environMap()
+
+	// Merge the given context
+	for k, v := range context {
+		data[k] = v
+	}
+
+	var buf bytes.Buffer
+	t, err := template.New("expandString").Parse(s)
+	if err != nil {
+		return "", err
+	}
+	t.Execute(&buf, data)
+
+	return buf.String(), nil
+}
+
+// environMap returns the current environment variables as a map.
+func environMap() map[string]string {
+	ret := make(map[string]string)
+	env := os.Environ()
+
+	for _, v := range env {
+		split := strings.SplitN(v, "=", 2)
+
+		if split[0] == "" && split[1] == "" {
+			continue
+		}
+
+		split[0] = strings.ToUpper(split[0]) // Normalize variable names to upper case
+		split[0] = strings.Replace(split[0], "(X86)", "_X86", -1)
+
+		ret[split[0]] = split[1]
+	}
+
+	return ret
 }
